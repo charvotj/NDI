686d61d54562d08c0ae4958d27da085ab6beb3c4
charvotj <jakub.charvot@centrum.cz>
2023-10-18 14:48:18 +0200

    spi nkej

---
+++ src/SPI/SPI.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:40:47 10/18/2023 
+-- Design Name: 
+-- Module Name:    SPI - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity SPI is
+    Port ( CS_b : in  STD_LOGIC;
+           SCLK : in  STD_LOGIC;
+           MOSI : in  STD_LOGIC;
+           MISO : out  STD_LOGIC);
+end SPI;
+
+architecture Behavioral of SPI is
+
+begin
+
+
+end Behavioral;
+
