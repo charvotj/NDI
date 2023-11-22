0d75d1c0ce9e53ee72c4e85adbd742b89878c597
Radek Kučera <Jarek@skaut.cz>
2023-11-22 16:08:50 +0100

    Arithmetic added

---
+++ src/arith_unit/arith_unit.vhd
@@ -0,0 +1,86 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+
+entity arith_unit is
+    Generic(
+	g_DATA_SIZE : natural := 1
+	);
+    Port ( clk      : in  STD_LOGIC; -- crystal clock
+           we_data_fr1 : in  STD_LOGIC;
+           we_data_fr2 : in  STD_LOGIC;
+           data_in : in  STD_LOGIC_VECTOR(g_DATA_SIZE-1 downto 0);
+
+           add_res : out  STD_LOGIC_VECTOR(g_DATA_SIZE-1 downto 0);
+           mul_res : out  STD_LOGIC_VECTOR(g_DATA_SIZE-1 downto 0)
+           );
+end arith_unit;
+
+architecture Behavioral of arith_unit is
+    ------------------------------
+    ---------- SIGNALS -----------
+    ------------------------------
+    signal sig_fr1_reg_q, sig_fr2_reg_q               : STD_LOGIC_VECTOR(g_DATA_SIZE-1 downto 0);
+    signal sig_mul_res_reg_d, sig_add_res_reg_d       : STD_LOGIC_VECTOR(g_DATA_SIZE-1 downto 0);
+    signal sig_mul_res_reg_en, sig_add_res_reg_en     : std_logic;
+
+begin
+
+    Data_fr1_reg : entity work.n_bit_register(Behavioral)
+    generic map (
+        g_DATA_SIZE => g_DATA_SIZE
+     )	
+    port map (
+      clk            => clk,
+      enable_reg     => we_data_fr1,
+      in_reg_d       => data_in,
+      out_reg_q      => sig_fr1_reg_q
+    );
+
+    Data_fr2_reg : entity work.n_bit_register(Behavioral)
+    generic map (
+        g_DATA_SIZE => g_DATA_SIZE
+     )	
+    port map (
+      clk            => clk,
+      enable_reg     => we_data_fr2,
+      in_reg_d       => data_in,
+      out_reg_q      => sig_fr2_reg_q
+    );
+
+    Data_add_res_reg : entity work.n_bit_register(Behavioral)
+    generic map (
+        g_DATA_SIZE => g_DATA_SIZE
+     )	
+    port map (
+      clk            => clk,
+      enable_reg     => sig_add_res_reg_en,
+      in_reg_d       => sig_add_res_reg_d,
+      out_reg_q      => add_res
+    );
+
+    Data_mul_res_reg : entity work.n_bit_register(Behavioral)
+    generic map (
+        g_DATA_SIZE => g_DATA_SIZE
+     )	
+    port map (
+      clk            => clk,
+      enable_reg     => sig_mul_res_reg_en,
+      in_reg_d       => sig_mul_res_reg_d,
+      out_reg_q      => mul_res
+    );
+
+    sig_add_res_reg_d <= std_logic_vector(unsigned(sig_fr1_reg_q) + unsigned(sig_fr2_reg_q));
+
+    
+                  
+    -- Add more output logic as needed
+  
+    ------------------------------
+    ---------- LOGIC -----------
+    ------------------------------
+
+
+
+end Behavioral;
---
+++ src/arith_unit/n_bit_register.vhd
@@ -0,0 +1,33 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.math_real.all;
+
+entity n_bit_register is
+    Generic(
+        g_DATA_SIZE : natural := 2
+        );
+    Port ( clk        : in STD_LOGIC;
+           enable_reg : in STD_LOGIC;
+           in_reg_d   : in  STD_LOGIC_VECTOR(g_DATA_SIZE-1 downto 0);
+           out_reg_q  : out  STD_LOGIC_VECTOR(g_DATA_SIZE-1 downto 0)
+    );
+
+end n_bit_register;
+
+architecture Behavioral of n_bit_register is -- 🤣
+    signal reg_d, reg_q : STD_LOGIC_VECTOR(g_DATA_SIZE-1 downto 0) := (others => '0');
+begin
+
+    process (clk)
+    begin
+        if rising_edge(clk) then 
+            if (enable_reg = '1') then
+                reg_q <= reg_d; 
+            end if;
+        end if;
+    end process;
+    reg_d <= in_reg_d;
+    out_reg_q <= reg_q;
+
+end architecture;
